5157b0ce42b7f821ff19a140877dfdab621d7b05
Gestin <gestin.remi@gmail.com>
2019-11-28 15:46:14 +0100

    knn test

---
+++ script.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors.classification import KNeighborsClassifier
+
+
+df = pd.read_csv("tutorial_data_eval_Sonar.csv", delimiter=';')
+
+# X = data without class
+X = df.values[:, 0:-1].astype(float)
+
+# y = just the class column
+y = df['Class'].copy()
+
+# we choose a test size of 30%, and not chosen randomly.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# Set up the knn model with default values
+clf = KNeighborsClassifier(n_neighbors=5, metric="minkowski", p=1)
+
+# Fit the model
+clf.fit(X_train, y_train)
+
+
+KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',
+                     metric_params=None, n_jobs=None, n_neighbors=5, p=1,
+                     weights='uniform')
+
+# launch the prediction
+predicted = clf.predict(X_test)
+
+# results
+print("Accuracy:")
+print(accuracy_score(y_test, predicted))
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, predicted))
+print("Classification Report:")
+print(classification_report(y_test, predicted))
