0894fb0fee6440eef980075f11930c37c6604ca8
Gestin <gestin.remi@gmail.com>
2019-11-29 13:48:34 +0100

    add logistic regression and decision tree

--- script.py
+++ script.py
@@ -3,7 +3,8 @@ import pandas as pd
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors.classification import KNeighborsClassifier
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 
 df = pd.read_csv("tutorial_data_eval_Sonar.csv", delimiter=';')
 
@@ -18,17 +19,41 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 # Set up the knn model with default values
 clf = KNeighborsClassifier(n_neighbors=5, metric="minkowski", p=1)
+logreg = LogisticRegression(solver="lbfgs")
+decisionTree = DecisionTreeClassifier()
+
 
 # Fit the model
 clf.fit(X_train, y_train)
+logreg.fit(X_train, y_train)
+decisionTree.fit(X_train, y_train)
 
 # launch the prediction
-predicted = clf.predict(X_test)
+y_pred_knn = clf.predict(X_test)
+y_pred_lr = logreg.predict(X_test)
+y_pred_tree = decisionTree.predict(X_test)
 
 # results
 print("Accuracy:")
-print(accuracy_score(y_test, predicted))
+print(accuracy_score(y_test, y_pred_knn))
 print("Confusion Matrix:")
-print(confusion_matrix(y_test, predicted))
+print(confusion_matrix(y_test, y_pred_knn))
 print("Classification Report:")
-print(classification_report(y_test, predicted))
+print(classification_report(y_test, y_pred_knn))
+
+
+# results
+print("Accuracy:")
+print(accuracy_score(y_test, y_pred_lr))
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred_lr))
+print("Classification Report:")
+print(classification_report(y_test, y_pred_lr))
+
+# results
+print("Accuracy:")
+print(accuracy_score(y_test, y_pred_tree))
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred_tree))
+print("Classification Report:")
+print(classification_report(y_test, y_pred_tree))
