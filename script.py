0bf042c04a556a90132a194b1389556e056247dc
Gestin <gestin.remi@gmail.com>
2019-11-30 10:50:43 +0100

    add the odd ratios

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
@@ -8,42 +9,35 @@ from sklearn.tree import DecisionTreeClassifier
 
 df = pd.read_csv("tutorial_data_eval_Sonar.csv", delimiter=';')
 
-# X = data without class
+# X = data without class column
 X = df.values[:, 0:-1].astype(float)
 
 # y = just the class column
 y = df['Class'].copy()
 
-# we choose a test size of 30%, and not chosen randomly.
+# we choose a test size of 20%, and not chosen randomly.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Set up the knn model with default values
-clf = KNeighborsClassifier(n_neighbors=5, metric="minkowski", p=1)
+# Set up different methods of predictions
+knn = KNeighborsClassifier(n_neighbors=5, metric="minkowski", p=1)
 logreg = LogisticRegression(solver="lbfgs")
 decisionTree = DecisionTreeClassifier()
 
-
-# Fit the model
-clf.fit(X_train, y_train)
+# Fit the models
+knn.fit(X_train, y_train)
 logreg.fit(X_train, y_train)
 decisionTree.fit(X_train, y_train)
 
-# launch the prediction
-y_pred_knn = clf.predict(X_test)
+# Get the odd ratios
+print("________ ODD RATIOS _________")
+print(np.exp(logreg.coef_))
+
+# Launch the predictions
+y_pred_knn = knn.predict(X_test)
 y_pred_lr = logreg.predict(X_test)
 y_pred_tree = decisionTree.predict(X_test)
 
-# results
-print("_______________KNN______________")
-print("Accuracy:")
-print(accuracy_score(y_test, y_pred_knn))
-print("Confusion Matrix:")
-print(confusion_matrix(y_test, y_pred_knn))
-print("Classification Report:")
-print(classification_report(y_test, y_pred_knn))
-
-
-# results
+# Results
 print("_______________LOGISTIC REGRESSION______________")
 print("Accuracy:")
 print(accuracy_score(y_test, y_pred_lr))
@@ -52,7 +46,14 @@ print(confusion_matrix(y_test, y_pred_lr))
 print("Classification Report:")
 print(classification_report(y_test, y_pred_lr))
 
-# results
+print("_______________KNN______________")
+print("Accuracy:")
+print(accuracy_score(y_test, y_pred_knn))
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred_knn))
+print("Classification Report:")
+print(classification_report(y_test, y_pred_knn))
+
 print("_______________DECISION TREE______________")
 print("Accuracy:")
 print(accuracy_score(y_test, y_pred_tree))
